5efc7744a36d0491646dba8327ab4ea190bc50b9
MiscEris <beatricegenilo@gmail.com>
2026-04-22 19:39:11 -0600

    add main menu in main method
    cases still need to be hooked up

--- src/app/AppDriver.java
+++ src/app/AppDriver.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import databaseController.DatabaseController;
 import manager.CheckoutManager;
@@ -19,10 +20,53 @@ public class AppDriver {
 
 		
 		new BookManager(dbc.getConn(), dbc.getStmt());
-
-
 		// new MemberManager(DatabaseConnection.getConn(), DatabaseConnection.getStmt());
 		// new CheckoutManager(DatabaseConnection.getConn(), DatabaseConnection.getStmt());
+		
+		
+		//Main Menu Method
+		int choiceM = 0;
+		//loop for entering sub-menus/methods
+		while(choiceM != 4) {
+			Scanner keyboardM = new Scanner(System.in);
+			System.out.println("1) Manage Books.");
+			System.out.println("2) Manage Members.");
+			System.out.println("3) Manage Checkouts.");
+			System.out.println("4) Exit program.");
+			choiceM = Integer.parseInt(keyboardM.nextLine());
+			switch(choiceM) {
+				case 1: 
+				try { 
+					keyboardM.close();
+					//use initialized books class
+				} catch (Throwable e) {
+					e.getMessage();
+				}
+					break;
+				case 2:
+				try {
+					keyboardM.close();
+					//use initialized members class
+				} catch (Throwable e) {
+					e.getMessage();
+				}
+					break;
+				case 3:
+				try {
+					keyboardM.close();
+					//use the initialized checkouts class
+				} catch (Throwable e) {
+					e.getMessage();
+				}
+					break;
+				case 4:
+					keyboardM.close();
+					System.out.println("Exiting program");
+					break;
+				default:
+					System.out.println("Invalid input. Please enter 1 to 4.");	
+			}
+		}
 	}
 
 }
