68fe68f91bf42e3f01facd1c71ca836f14b307c6
Doug Dickens <hellsmaximum@gmail.com>
2026-04-22 21:09:07 -0600

    merge new app driver code Made new exception for dates Added new/better comments Added some view all error handling Made CheckoutManager

--- src/app/AppDriver.java
+++ src/app/AppDriver.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import databaseController.DatabaseController;
 import manager.CheckoutManager;
@@ -10,19 +11,57 @@ import manager.BookManager;
 public class AppDriver {
 
 	public static void main(String[] args) {
+		// Establish connection to the database
 		DatabaseController dbc = new DatabaseController();
 		try {
 			dbc.connect();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 		
-		new BookManager(dbc.getConn(), dbc.getStmt());
-
-
-		// new MemberManager(DatabaseConnection.getConn(), DatabaseConnection.getStmt());
-		// new CheckoutManager(DatabaseConnection.getConn(), DatabaseConnection.getStmt());
+		//Main Menu
+		int choiceM = 0;
+		Scanner keyboardM = new Scanner(System.in);
+		//loop for entering sub-menus/methods
+		while(choiceM != 4) {
+			System.out.println("1) Manage Books.");
+			System.out.println("2) Manage Members.");
+			System.out.println("3) Manage Checkouts.");
+			System.out.println("4) Exit program.");
+			choiceM = Integer.parseInt(keyboardM.nextLine());
+			switch(choiceM) {
+				case 1: 
+				try { 
+					new BookManager(dbc.getConn(), dbc.getStmt());
+					//use initialized books class
+				} catch (Throwable e) {
+					System.out.println(e.getMessage());
+				}
+					break;
+				case 2:
+				try {
+					new MemberManager(dbc.getConn(), dbc.getStmt());
+					//use initialized members class
+				} catch (Throwable e) {
+					System.out.println(e.getMessage());
+				}
+					break;
+				case 3:
+				try {
+					new CheckoutManager(dbc.getConn(), dbc.getStmt());
+					//use the initialized checkouts class
+				} catch (Throwable e) {
+					System.out.println(e.getMessage());
+				}
+					break;
+				case 4:
+					keyboardM.close();
+					System.out.println("Exiting program");
+					break;
+				default:
+					System.out.println("Invalid input. Please enter 1 to 4.");	
+			}
+		}
 	}
 
 }
---
+++ src/errors/InvalidDateException.java
@@ -0,0 +1,22 @@
+package errors;
+
+public class InvalidDateException extends Throwable{
+    // error message variable
+    private String error;
+
+    // constructor with no parameters that sets a default error message and a constructor with a string parameter to set a custom error message
+    public InvalidDateException() {
+        this.error = "Invalid date format. Please enter the date in the format YYYY-MM-DD.";
+    }
+
+    // constructor with a string parameter to set a custom error message based on the user's input
+    public InvalidDateException(String error) {
+        this.error = error;
+    }
+
+    // Override the getMessage method to return the error message when the exception is caught
+    @Override
+    public String getMessage() {
+        return error;
+    }
+}
